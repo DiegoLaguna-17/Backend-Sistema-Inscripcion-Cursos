37bec0620f3e20d8a8565f20dc72956472180b98
Adriana-Alvarez <adriana.alvarez.c@ucb.edu.bo>
2026-02-16 19:13:34 -0400

    Conexión con supabase

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
+
+// Cargamos las variables de entorno
+require('dotenv').config();
+
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Backend funcionando');
+  res.json({ 
+    message: 'Backend funcionando',
+    supabase_conectado: true,
+    supabase_url: process.env.SUPABASE_URL ? 'Configurada' : 'No configurada'
+  });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
