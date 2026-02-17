6dece088be4a24e8664f01d65689cd110f20f000
Rosario Calisaya <misa26amane@gmail.com>
2026-02-17 17:56:14 -0400

    [IMP] agregando verificaciones en las rutas de endpoints para el CRUD estudiantes por parte del de seguridad

--- src/routes/estudiante.routes.js
+++ src/routes/estudiante.routes.js
@@ -1,12 +1,51 @@
 const { Router } = require("express");
-const controller = require("../controllers/estudiante.controller");
+const estudianteController = require("../controllers/estudiante.controller");
+
+const {
+    verificarAutenticacion,
+    verificarPermiso,
+} = require("../middlewares/auth.middleware");
 
 const router = Router();
 
-router.post("/", controller.registrarEstudiante);       // Registrar estudiante
-router.get("/", controller.listarEstudiantes);          // Mostrar lista de estudiantes
-router.get("/:ci", controller.obtenerEstudiantePorCI);  // Obtener estudiante por CI
-router.put("/:ci", controller.actualizarEstudiante);    // Editar estudiante
-router.delete("/:ci", controller.eliminarEstudiante);   // Eliminar estudiante
+// Crear estudiante
+router.post(
+    "/",
+    verificarAutenticacion,
+    verificarPermiso("registro de usuarios"),
+    estudianteController.registrarEstudiante
+);
+
+// Modificar estudiante
+router.put(
+  "/:ci",
+  verificarAutenticacion,
+  verificarPermiso("registro de usuarios"),
+  estudianteController.actualizarEstudiante
+);
+
+// Eliminar estudiante
+router.delete(
+  "/:ci",
+  verificarAutenticacion,
+  verificarPermiso("registro de usuarios"),
+  estudianteController.eliminarEstudiante
+);
+
+// Listar estudiantes
+router.get(
+    "/",
+    verificarAutenticacion,
+    verificarPermiso("registro de usuarios"),
+    estudianteController.listarEstudiantes
+);
+
+// Obtener por CI
+router.get(
+    "/:ci",
+    verificarAutenticacion,
+    verificarPermiso("registro de usuarios"),
+    estudianteController.obtenerEstudiantePorCI
+);
 
 module.exports = router;
