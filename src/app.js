d7fe75cfd30a846880267e640f5def31fec92a02
DiegoLaguna-17 <diego.laguna@ucb.edu.bo>
2026-03-17 13:50:14 -0400

    Prueba de errores correos

--- src/app.js
+++ src/app.js
@@ -26,6 +26,24 @@ app.use('/api', routes);
 // Middleware de manejo de errores
 app.use(errorMiddleware);
 
+app.get("/test-mail-simple", async (req, res) => {
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: "tuemail@gmail.com",
+      subject: "Test simple",
+      text: "Sin puppeteer 🚀"
+    });
+
+    console.log(info);
+    res.send("Correo OK");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
+  }
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
